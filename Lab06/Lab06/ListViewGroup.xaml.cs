be61bead2459ec3d745992f116dffde141b77fb1
Cristina <ana.mendoza@tecsup.edu.pe>
2023-10-13 15:29:33 -0500

    subiendo

--- Lab06/Lab06/ListViewGroup.xaml.cs
+++ Lab06/Lab06/ListViewGroup.xaml.cs
@@ -21,13 +21,11 @@ namespace Lab06
         {
             InitializeComponent();
             EstudianteView.ItemsSource = estudiantes;
-            estudiantes.Add(new Estudiante { DisplayName = "Rob Finnerty" });
-            estudiantes.Add(new Estudiante { DisplayName = "Bill Wrestler" });
-            estudiantes.Add(new Estudiante { DisplayName = "Dr. Geri-Beth Hooper " });
-            estudiantes.Add(new Estudiante { DisplayName = "Dr.Keith Joyce-Purdy" });
-            estudiantes.Add(new Estudiante { DisplayName = "Sheri Spruce" });
-            estudiantes.Add(new Estudiante { DisplayName = "Burt Indybrick" });
+            estudiantes.Add(new Estudiante { DisplayName = "Sección A" });
+            estudiantes.Add(new Estudiante { DisplayName = "Sección B" });
+            estudiantes.Add(new Estudiante { DisplayName = "Sección C " });
+
 
         }
     }
-    }S
+    }
